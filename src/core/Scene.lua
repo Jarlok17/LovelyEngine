543a4c639cf143a391218d81951ca0a9a48ec58a
Jarlok17 <abilik48@gmail.com>
2025-09-17 17:03:53 +0300

    feat: add Scene.lua

---
+++ src/core/Scene.lua
@@ -0,0 +1,49 @@
+-- src/core/Scene.lua
+local Scene = {}
+Scene.__index = Scene
+
+function Scene.new()
+	local self = setmetatable({}, Scene)
+	self.name = "base_scene"
+	self.isActive = true
+	self.isTransparent = false
+	return self
+end
+
+function Scene:enter(transitionData)
+	-- Virtual method override in other classes
+end
+
+function Scene:exit()
+	-- Virtual method override in other classes
+end
+
+function Scene:pause()
+	self.isActive = false
+end
+
+function Scene:resume(transitionData)
+	self.isActive = true
+end
+
+function Scene:update(dt)
+	-- Virtual method override in other classes
+end
+
+function Scene:draw()
+	-- Virtual method override in other classes
+end
+
+-- Базові обробники подій Love2D
+function Scene:keypressed(key, scancode, isrepeat) end
+function Scene:keyreleased(key, scancode) end
+function Scene:textinput(text) end
+function Scene:mousemoved(x, y, dx, dy, istouch) end
+function Scene:mousepressed(x, y, button, istouch, presses) end
+function Scene:mousereleased(x, y, button, istouch, presses) end
+function Scene:wheelmoved(x, y) end
+function Scene:focus(focused) end
+function Scene:quit() end
+function Scene:resize(w, h) end
+
+return Scene
